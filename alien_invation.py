20674e682a16be40be60fb582b33a6919f68ebd4
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2020-12-26 18:45:57 +0700

    Implement ship render in main

--- alien_invation.py
+++ alien_invation.py
@@ -1,16 +1,19 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
 
 class AlienInvation:
 
     def __init__(self):
         pygame.init()
-        self.settings = Settings
+        self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invation")
 
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
@@ -20,11 +23,11 @@ class AlienInvation:
                     sys.exit()
 
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             pygame.display.flip()
 
 
 if __name__ == '__main__':
-
     ai = AlienInvation()
     ai.run_game()
--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ class Settings:
         """
 
         # screen setting
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
