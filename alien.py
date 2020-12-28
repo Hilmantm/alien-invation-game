3314956a50f2ce61c2c582367f898316cadb5043
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2020-12-28 17:45:39 +0700

    add setting for alien object and add function for check if alien touch the edge of the screen and update the alien position

--- alien.py
+++ alien.py
@@ -10,6 +10,7 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load alien image and set rect attribute
         self.image = pygame.image.load('./images/alien.bmp')
@@ -21,3 +22,20 @@ class Alien(Sprite):
 
         # Store to exact position
         self.x = float(self.rect.x)
+
+    def update(self):
+        """
+        Move the alien to right or to left
+        :return:
+        """
+        self.x = (self.settings.alien_speed + self.settings.fleet_direction)
+        self.rect.x = self.x
+
+    def check_edges(self):
+        """
+        Return true if alien touch the edge of the screen
+        :return:
+        """
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
