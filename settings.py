c757226812595d4c9cdc5c5430fc373a1d606c9b
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2020-12-28 13:21:04 +0700

    add setting for bullet and create new object file bullet

--- settings.py
+++ settings.py
@@ -9,7 +9,13 @@ class Settings:
         # screen setting
         self.screen_width = 800
         self.screen_height = 600
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (230, 230, 230)  # RGB CODE
 
         # ship setting
         self.ship_speed = 1.5
+
+        # bullet setting
+        self.bullet_speed = 1.0
+        self.bulled_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)  # RGB CODE
