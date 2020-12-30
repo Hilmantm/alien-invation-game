61df220750084ac0f96761d5bc5dc405d3711e8c
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2020-12-30 12:19:26 +0700

    add score and level in game_stats

---
+++ scoreboard.py
@@ -0,0 +1,55 @@
+import pygame
+from pygame.sprite import Group
+from ship import Ship
+
+
+class Scoreboard:
+    """
+    Class to handle score information
+    """
+
+    def __init__(self, ai_game):
+        self.ai_game = ai_game
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+        self.settings = ai_game.settings
+        self.stats = ai_game.stats
+
+        # font setting for scoring information
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+
+        self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
+        self.prep_ship()
+
+    def prep_score(self):
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
+        self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
+
+        # display the score in top right of the screen
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def prep_high_score(self):
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+
+        self.high_score_rect = self.high_score_rect.get_rect()
+        self.high_score_rect.centers = self.screen_rect.centers
+        self.high_score_rect.top = self.score_rect.top
+
+    def prep_level(self):
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
+
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
+    def prep_ship(self):
+        pass
